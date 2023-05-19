743674a6e4dd0aa4f743dc3091d991b7b28233ad
Bradley Hagger <brad_2nd@hotmail.com>
2023-05-19 14:51:38 +0100

    Updates

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const connectDB = require("./config/db")
+const exphbs = require("express-handlebars")
+const morgan = require("morgan")
+
+// Load Config
+dotenv.config({path: "./config/config.env"})
+
+connectDB()
+
+const app = express()
+
+// Logging
+if(process.env.NODE.ENV == "development"){
+    app.use(morgan("dev"))
+}
+
+// Handlebars
+app.engine(".hbs", exphbs.engine({extname: ".hbs"}))
+app.set("view engine", ".hbs")
+
+const PORT = process.env.port || 3000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
